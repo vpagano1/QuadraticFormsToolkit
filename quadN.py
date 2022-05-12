795c7ac2fbfdb4797a27d7ec362df3709db7090a
Max Gotts <mgotts@princeton.edu>
2022-05-12 15:03:45 -0700

    non-diagonal matrices implemented! Switch over to quadN instead of diagN for this functionality

--- quadN.py
+++ quadN.py
@@ -1,18 +1,15 @@
 from math import sqrt, ceil
 from sys import exit
 from sympy import primerange
+import numpy as np
 
-def evalQuad(coeff,entries):
-    rank = len(coeff)
+def evalQuad(quad,entries):
+    rank = len(quad)
     # if rank > 1:
-    if rank != len(entries):
-        raise Exception("Length of coefficients must match length of entries.")
-    n = 0
-    for i in range(rank):
-        n += coeff[i]*entries[i]**2
-    return n
+    if rank != len(entries): raise Exception("Number of variables must match length of entries.")
+    return np.matmul(entries,np.matmul(quad,np.transpose(entries)))
 
-def makeZeros(n):
+def make_zeros(n):
     zeros = []
     for i in range(n):
         zeros.append(0)
@@ -29,45 +26,72 @@ def clock(watch,m):
             break
     return watch
 
-def represents(coeff,k):
+def represents(quad,k):
     v = max(ceil(sqrt(k)),1)
-    rank = len(coeff)
-    entries = makeZeros(rank)
+    rank = len(quad)
+    entries = make_zeros(rank)
     while entries != True:
-        if evalQuad(coeff, entries) == k:
+        if evalQuad(quad, entries) == k:
             return (True, entries)
         try: entries = clock(entries,v)
         except IndexError: exit()
     return (False, -1)
 
-def universal(coeff):
+def universal(quad):
     for k in range(16):
-        if not represents(coeff,k)[0]:
+        if not represents(quad,k)[0]:
             print(k,"fails")
             return False
     return True
 
-def get_coeffs():
+def get_quad():
     empty = False
-    coeff = []
+    diag = []
     while not empty:
-        new_coeff = input("#> ")
-        empty = new_coeff==""
+        new_diag = input("#> ")
+        empty = new_diag==""
         if not empty:
             try:
-                int_coeff = int(new_coeff)
-                coeff.append(int_coeff)
+                int_diag = int(new_diag)
+                diag.append(int_diag)
             except ValueError:
                 print("Integers only, please!")
-    return coeff
+    quad = []
+    for i in range(len(diag)):
+        quad.append(make_zeros(len(diag)))
+    for i in range(len(diag)): quad[i][i] = diag[i]
+    for i in range(len(diag)):
+        for j in range(i+1,len(diag)):
+            quad[i][j] = "X"
+            print_quad(quad)
+            new_entry = get_integer("##> ",0) # (r"+str(i+1)+",c"+str(j+1)+")
+            quad[i][j] = new_entry
+            quad[j][i] = new_entry
+    # for i in range(len(diag)):
+    #     for j in range(i):
+    #         # print(" at r"+str(i)+",c"+str(j)+"; replacing "+str(quad[i][j])+" with "+str(quad[j][i]))
+    #         quad[i][j] = quad[j][i]
+    print("Quadratic form is:")
+    print_quad(quad)
+    return quad
 
-def get_integer(input_string, default=False):
+def print_quad(quad):
+    for i in range(len(quad)):
+        print("[", end="")
+        for j in range(len(quad)):
+            if j != len(quad)-1:
+                print(quad[i][j], end=" ")
+            else:
+                print(quad[i][j], end="]\n")
+            
+
+def get_integer(input_string, default=None):
     notinteger = True
     while notinteger:
         try:
             notinteger = False
             stdin = input(input_string)
-            if stdin == "" and default != False: k = default
+            if stdin == "" and default != None: k = default
             else: k = int(stdin)
         except ValueError:
             notinteger = True
@@ -88,7 +112,7 @@ def square2class(k):
     for m in [1,3,5,7]:
         represented_bool = False
         for i in range(k+1):
-            rep = represents(qform, 8*i+m)
+            rep = represents(quad, 8*i+m)
             if rep[0]:
                 print(" u"+str(m)+":",8*i+m,"represented!",rep[1],"(n="+str(i)+": 8*"+str(i)+"+"+str(m)+")")
                 represented_bool = True 
@@ -98,7 +122,7 @@ def square2class(k):
 
         represented_bool = False
         for i in range(k+1):
-            rep = represents(qform, 2*(8*i+m))
+            rep = represents(quad, 2*(8*i+m))
             if rep[0]:
                 print("2u"+str(m)+":",2*(8*i+m),"represented!",rep[1],"(n="+str(i)+": 2*(8*"+str(i)+"+"+str(m)+") )")
                 represented_bool = True
@@ -113,7 +137,7 @@ def psquareclass(p,k):
     for i in range(k+1):
         if represented_bool == True: break
         for j in squares:
-            rep = represents(qform, p*i+j)
+            rep = represents(quad, p*i+j)
             if rep[0]:
                 print(" [1]:",p*i+j,"represented!",rep[1],"(n="+str(i)+": "+str(p)+"*"+str(i)+"+"+str(j)+")")
                 represented_bool = True
@@ -125,7 +149,7 @@ def psquareclass(p,k):
     for i in range(k+1):
         if represented_bool == True: break
         for j in antisquares:
-            rep = represents(qform, p*i+j)
+            rep = represents(quad, p*i+j)
             if rep[0]:
                 print(" [u]:",p*i+j,"represented!",rep[1],"(n="+str(i)+": "+str(p)+"*"+str(i)+"+"+str(j)+")")
                 represented_bool = True
@@ -137,7 +161,7 @@ def psquareclass(p,k):
     for i in range(k+1):
         if represented_bool == True: break
         for j in squares:
-            rep = represents(qform, p*(p*i+j))
+            rep = represents(quad, p*(p*i+j))
             if rep[0]:
                 print(" [p]:",p*(p*i+j),"represented!",rep[1],"(n="+str(i)+": "+str(p)+"*("+str(p)+"*"+str(i)+"+"+str(j)+") )")
                 represented_bool = True
@@ -149,7 +173,7 @@ def psquareclass(p,k):
     for i in range(k+1):
         if represented_bool == True: break
         for j in antisquares:
-            rep = represents(qform, p*(p*i+j))
+            rep = represents(quad, p*(p*i+j))
             if rep[0]:
                 print("[pu]:",p*(p*i+j),"represented!",rep[1],"(n="+str(i)+": "+str(p)+"*("+str(p)+"*"+str(i)+"+"+str(j)+") )")
                 represented_bool = True
@@ -160,36 +184,42 @@ def psquareclass(p,k):
 restart = True
 while True:
     if restart:
-        qform = get_coeffs()
+        quad = get_quad()
         restart = False
     n = input("> ")
     if n == "":
         pass
+    elif n in ["eval", "e", "ev", "evaluate"]:
+        entries = []
+        for i in range(len(quad)): entries.append(get_integer(">> "))
+        print(evalQuad(quad,entries))
     elif n in ["p", "print"]:
-        print(qform)
+        print_quad(quad)
     elif n in ["universal", "u", "uni"]:
-        if universal(qform): print("Universal!")
+        if universal(quad): print("Universal!")
     elif n in ["quit","q","exit","done"]:
         restart = True
     elif n in ["list", "l", "until", "unt"]:
         k = get_integer(">> ")
         for i in range(k+1):
-            rep = represents(qform, i)
+            rep = represents(quad, i)
             if rep[0]:
                 print(i,"represented!",rep[1])
             elif not rep[0]:
                 print(i,"not represented!")
     elif n in ["list hide", "hide list", "lh", "hl", "hide until", "until hide", "hunt", "unth"]:
         k = get_integer(">> ",1000)
-        for i in range(k+1):
-            rep = represents(qform, i)
-            if not rep[0]:
-                print(i,"not represented!")
+        try:
+            for i in range(k+1):
+                rep = represents(quad, i)
+                if not rep[0]:
+                    print(i,"not represented!")
+        except KeyboardInterrupt: pass
     elif n in ["u1","u3","u5","u7"]:
         k = get_integer(">> ",30)
         m = int(n[1])
         for i in range(k+1):
-            rep = represents(qform, 8*i+m)
+            rep = represents(quad, 8*i+m)
             if rep[0]:
                 print(8*i+m,"represented!",rep[1],"(n="+str(i)+": 8*"+str(i)+"+"+str(m)+")")
                 break
@@ -197,7 +227,7 @@ while True:
         k = get_integer(">> ",30)
         m = int(n[2])
         for i in range(k+1):
-            rep = represents(qform, 2*(8*i+m))
+            rep = represents(quad, 2*(8*i+m))
             if rep[0]:
                 print(2*(8*i+m),"represented!",rep[1],"(n="+str(i)+": 2*(8*"+str(i)+"+"+str(m)+") )")
                 break
@@ -210,9 +240,8 @@ while True:
         psquareclass(p, k)
     elif n in ["sq", "squares", "sqs", "square classes"]:
         k = get_integer(">> ",30)
-        if len(qform)>=3:
-            det = 1
-            for c in qform: det *= c
+        if len(quad)>=3:
+            det = np.linalg.det(quad)
             print("p=2")
             square2class(k)
             for p in primerange(3,det):
@@ -220,7 +249,7 @@ while True:
                     print()
                     print("p="+str(p))
                     psquareclass(p,k)
-        if len(qform)==2:
+        if len(quad)==2:
             pmax = get_integer(">> ",30)
             print("p=2")
             square2class(k)
@@ -243,7 +272,7 @@ while True:
         k = get_integer(">> ",5)
         try:
             for i in range(k+1):
-                rep = represents(qform, n*p**i)
+                rep = represents(quad, n*p**i)
                 if rep[0]:
                     print(str(n)+"*"+str(p)+"^"+str(i),"=",n*p**i,"represented!",rep[1])
                 elif not rep[0]:
@@ -259,7 +288,7 @@ while True:
         for n in range(1,m+1):
             try:
                 for i in range(k+1):
-                    rep = represents(qform, n*p**i)
+                    rep = represents(quad, n*p**i)
                     if rep[0]:
                         print(str(n)+"*"+str(p)+"^"+str(i),"=",n*p**i,"represented!",rep[1])
                     elif not rep[0]:
@@ -268,7 +297,7 @@ while True:
             if n != m: print()
     elif n in ["city","c","main"]:
         det = 1
-        for c in qform: det *= c
+        for c in quad: det *= c
         Lambda = 8
         for p in primerange(3,det):
             if det%p == 0: Lambda *= p
@@ -288,7 +317,7 @@ while True:
     else:
         try:
             nint = int(n)
-            rep = represents(qform, nint)
+            rep = represents(quad, nint)
             if rep[0]:
                 print("Represented!", rep[1])
             elif not rep[0]:
